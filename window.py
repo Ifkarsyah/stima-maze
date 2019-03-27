75af39fa869c1ebfa51aa380e299cf5e2dc7fc0e
Ferdian Ifkarsyah <ferdian.ifkarsyah@gmail.com>
2019-03-27 18:19:52 +0700

    refactor

--- window.py
+++ window.py
@@ -1,7 +1,6 @@
 import turtle
 import time
-from algo import maze, Point, pos_start, pos_finish, path
-# MODELS
+from algo import maze
 
 
 class Block(turtle.Turtle):
@@ -14,70 +13,45 @@ class Block(turtle.Turtle):
 
 
 class Player(turtle.Turtle):
-    def __init__(self, point_init):
+    def __init__(self, x, y):
         turtle.Turtle.__init__(self)
         self.shape("square")
         self.color("blue")
         self.penup()
-        self.pos = point_init
+        self.x, self.y = x, y
         self.speed(0)
 
 
 class Trail(turtle.Turtle):
-    def __init__(self, point_init):
+    def __init__(self, x, y):
         turtle.Turtle.__init__(self)
         self.shape("circle")
         self.shapesize(0.2, 0.2)
         self.color("blue")
         self.penup()
-        self.pos = point_init
+        self.x, self.y = x, y
         self.speed(0)
 
 
-# WINDOWS
 wn = turtle.Screen()
 wn.bgcolor("black")
 wn.title("Stima Maze")
 wn.setup(700, 700)
 
 
-# DRAW
 def draw_maze(maze):
-    global block
+    block = Block()
     for y in range(len(maze)):
         for x in range(len(maze[y])):
-            char_code = maze[y][x]
-            screen_x = -288 + (x*24)
-            screen_y = 288 - (y*24)
-            if char_code == "1":
-                block.goto(screen_x, screen_y)
+            sc_x, sc_y = -288 + (x*24), 288 - (y*24)
+            if maze[y][x] == "1":
+                block.goto(sc_x, sc_y)
                 block.stamp()
 
 
-def draw_player(p):
-    screen_x = -288 + (p.pos.x*24)
-    screen_y = 288 - (p.pos.y*24)
-    p.goto(screen_x, screen_y)
-
-
-def draw_path(path, p):
-    for pos in path:
-        t = Trail(p.pos)
-        screen_x = -288 + (pos.x*24)
-        screen_y = 288 - (pos.y*24)
-        p.goto(screen_x, screen_y)
-        t.goto(screen_x, screen_y)
-        t.stamp()
-        time.sleep(0.5)
-
-
-block = Block()
-player = Player(pos_start)
-
-# draw
+player = Player(0, 1)
 draw_maze(maze)
-draw_player(player)
-draw_path(path, player)
+
 
 while True:
     pass
