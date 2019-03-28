0bc64e095d81e3f865d68cf69a3a5094361e336a
Ferdian Ifkarsyah <ferdian.ifkarsyah@gmail.com>
2019-03-28 12:29:22 +0700

    add utils.py search_door

--- utils.py
+++ utils.py
@@ -2,8 +2,14 @@ maze = open('maze.txt', 'r').read().splitlines()
 
 
 def search_door(maze):
-    # SAMPLE
-    return [(0, 11), (30, 30)]
+    rows = len(maze)
+    cols = len(maze[0])
+    doors = [(x, 0) for x in range(rows) if maze[0][x] == '0']
+    doors += [(x, cols-1) for x in range(rows) if maze[cols-1][x] == '0']
+    doors += [(0, y) for y in range(cols) if maze[y][0] == '0']
+    doors += [(rows-1, y) for y in range(cols) if maze[y][rows-1] == '0']
+    return set(doors)
 
 
 start, finish = search_door(maze)
+# Sample Result: start = (0,11); finish = (40,27)
