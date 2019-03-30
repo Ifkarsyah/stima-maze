692c0245d3fd61cd9c454849d3e6b65bd12b4b0b
Muhammad Hendry Prasetya <45161697+hendpraz@users.noreply.github.com>
2019-03-30 13:42:52 +0700

    Add start&finish, add reset_path
    draw_maze separated into draw_maze and draw_path. Resetting path after 5 secs (with blackblock class). Add start & finish class (similar to player class) with color red & green.

--- window.py
+++ window.py
@@ -13,6 +13,14 @@ class Block(Turtle):
         self.penup()
         self.speed(0)
 
+class BlackBlock(Turtle):
+    def __init__(self):
+        Turtle.__init__(self)
+        self.shape("square")
+        self.color("black")
+        self.shapesize(0.5)
+        self.penup()
+        self.speed(0)
 
 class Player(Turtle):
     def __init__(self, point):
@@ -24,37 +32,78 @@ class Player(Turtle):
         self.x, self.y = point
         self.speed(0)
 
+class Start(Turtle):
+    def __init__(self, point):
+        Turtle.__init__(self)
+        self.shape("square")
+        self.color("red")
+        self.shapesize(0.5)
+        self.penup()
+        self.x, self.y = point
+        self.speed(0)
+
+class Finish(Turtle):
+    def __init__(self, point):
+        Turtle.__init__(self)
+        self.shape("square")
+        self.color("green")
+        self.shapesize(0.5)
+        self.penup()
+        self.x, self.y = point
+        self.speed(0)
 
 wn = Screen()
 wn.bgcolor("black"), wn.title("Stima Maze"), wn.setup(1000, 1000)
 
-
 def pos_to_sc(x, y):
     return (-432 + (x * 12), 312 - (y * 12))
 
-
-def draw_maze(maze, pathway):
+def draw_maze(maze):
     b = Block()
-    p = Player(pathway[0])
     for y in range(len(maze)):
         for x in range(len(maze[y])):
             if maze[y][x] == 1:
                 b.goto(pos_to_sc(x, y))
                 b.stamp()
-    for path in pathway:
-        x, y = path
-        p.goto(pos_to_sc(x, y))
-        p.stamp()
 
+def draw_path(pathway):
+    #Draw Start and Finish
+    s = Start(pathway[0])
+    x, y = pathway[0];
+    s.goto(pos_to_sc(x, y))
+    s.stamp()
+
+    f = Finish(pathway[len(pathway)-1])
+    x, y = pathway[len(pathway)-1];
+    f.goto(pos_to_sc(x, y))
+    f.stamp()
+
+    #Draw player movement
+    p = Player(pathway[0])
+    for path in pathway:
+        if((path != pathway[0]) and (path != pathway[len(pathway)-1])):
+            x, y = path
+            p.goto(pos_to_sc(x, y))
+            p.stamp()
+
+def reset_path(pathway):
+    #Reset player movement
+    p = BlackBlock()
+    for path in pathway:
+        if((path != pathway[0]) and (path != pathway[len(pathway)-1])):
+            x, y = path
+            p.goto(pos_to_sc(x, y))
+            p.stamp()
 
 path_astar = [(y, x) for (x, y) in jalurAStar]
 path_bfs = [(y, x) for (x, y) in bfs(start, finish)]
 print("THIS IS BFS PATH\n")
-draw_maze(maze, path_bfs)
+draw_maze(maze)
+draw_path(path_bfs)
 time.sleep(5)
+reset_path(path_bfs)
 
-wn.reset()
 print("THIS IS A* PATH")
-draw_maze(maze, path_astar)
+draw_path(path_astar)
 
 wn.exitonclick()
