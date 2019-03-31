b6a1d15d3eea98cd4a17868054c4dbc5b7d6e301
Ferdian Ifkarsyah <ferdian.ifkarsyah@gmail.com>
2019-03-31 10:50:42 +0700

    validated start and finish point

--- utils.py
+++ utils.py
@@ -1,34 +1,6 @@
 from sys import argv
 
 
-def read_matrix(filename):  # --> matrix of int --> [[1,...],[0,1,...],...]
-    with open(filename, 'r') as f:
-        maze = [list(line.strip()) for line in f.readlines()]
-        maze = [[int(c) for c in maze[r]] for r in range(len(maze))]
-    return maze
-
-
-maze = read_matrix(argv[1])
-rows, cols = len(maze), len(maze[0])
-
-
-#def search_door():  # hole in the border of maze --> (1,0), (9,10)
-#    doors = [(0, c) for c in range(cols) if maze[0][c] == 0]
-#    doors += [(rows - 1, c) for c in range(cols) if maze[rows - 1][c] == 0]
-#    doors += [(r, 0) for r in range(rows) if maze[r][0] == 0]
-#    doors += [(r, cols - 1) for r in range(rows) if maze[r][cols - 1] == 0]
-#    return sorted(set(doors))
-
-
-#start, finish = search_door()  # hole in the border of maze --> (1,0), (9,10)
-print("Masukkan posisi awal :")
-inp = input().split(" ")
-start = (int(inp[0]),int(inp[1]))
-
-print("Masukkan posisi akhir :")
-inp = input().split(" ")
-finish = (int(inp[0]),int(inp[1]))
-
 def pos_add(a, b):  # pos_add((1,1),(1,2)) --> (2,3)
     return tuple(map(sum, zip(a, b)))
 
@@ -41,3 +13,47 @@ def in_range(node):  # check if (r,c) still in maze --> bool
 def neighbors(node):  # neighbors((1,0)) --> [(1, 1), (2, 0), (0, 0)]
     ways = [pos_add(node, w) for w in [(0, 1), (0, -1), (1, 0), (-1, 0)]]
     return list(filter(in_range, ways))
+
+
+def read_matrix(filename):  # --> matrix of int --> [[1,...],[0,1,...],...]
+    with open(filename, 'r') as f:
+        maze = [list(line.strip()) for line in f.readlines()]
+        maze = [[int(c) for c in maze[r]] for r in range(len(maze))]
+    return maze
+
+
+maze = read_matrix(argv[1])
+rows, cols = len(maze), len(maze[0])
+
+
+# def search_door():  # hole in the border of maze --> (1,0), (9,10)
+#    doors = [(0, c) for c in range(cols) if maze[0][c] == 0]
+#    doors += [(rows - 1, c) for c in range(cols) if maze[rows - 1][c] == 0]
+#    doors += [(r, 0) for r in range(rows) if maze[r][0] == 0]
+#    doors += [(r, cols - 1) for r in range(rows) if maze[r][cols - 1] == 0]
+#    return sorted(set(doors))
+
+
+# start, finish = search_door()  # hole in the border of maze --> (1,0), (9,10)
+def valid(start):  # or finish
+    x, y = start
+    return in_range(start) and maze[x][y] == 0
+
+
+print("Masukkan posisi awal :")
+while True:
+    inp = input().split(" ")
+    start = (int(inp[0]), int(inp[1]))
+    if valid(start):
+        break
+    else:
+        print('Input bukan blok kosong atau diluar maze')
+
+print("Masukkan posisi akhir :")
+while True:
+    inp = input().split(" ")
+    finish = (int(inp[0]), int(inp[1]))
+    if valid(finish):
+        break
+    else:
+        print('Input bukan blok kosong atau diluar maze')
--- window.py
+++ window.py
@@ -2,11 +2,10 @@ from turtle import Turtle, Screen
 from astar import jalurAStar
 from bfs import bfs
 from utils import maze, rows, cols, start, finish
-import time
 
-#Block class, for walls
+
 class Block(Turtle):
-    def __init__(self,color1):
+    def __init__(self, color1):
         Turtle.__init__(self)
         self.shape("square")
         self.color(color1)
@@ -14,22 +13,14 @@ class Block(Turtle):
         self.penup()
         self.speed(0)
 
-#Route class, for drawing route
-#class Route(Turtle):
-#    def __init__(self, point):
-#        Turtle.__init__(self)
-#        self.shape("square")
-#        self.color("blue")
-#        self.shapesize(0.5)
-#        self.penup()
-#        self.x, self.y = point
-#        self.speed(0)
 
 wn = Screen()
 wn.bgcolor("black"), wn.title("Stima Maze"), wn.setup(1000, 1000)
 
+
 def pos_to_sc(x, y):
     return (-432 + (x * 12), 312 - (y * 12))
+
 
 def draw_maze(maze):
     b = Block("white")
@@ -39,33 +30,36 @@ def draw_maze(maze):
                 b.goto(pos_to_sc(x, y))
                 b.stamp()
 
+
 def draw_path(pathway):
-    #Draw Start and Finish
+    # Draw Start and Finish
     s = Block("red")
-    x, y = pathway[0];
+    x, y = pathway[0]
     s.goto(pos_to_sc(x, y))
     s.stamp()
 
     f = Block("green")
-    x, y = pathway[len(pathway)-1];
+    x, y = pathway[len(pathway) - 1]
     f.goto(pos_to_sc(x, y))
     f.stamp()
 
-    #Draw the route
-    #p = Route(pathway[0])
+    # Draw the route
+    # p = Route(pathway[0])
     p = Block("blue")
-    for i in range(1 , len(pathway)-1):
+    for i in range(1, len(pathway) - 1):
         x, y = pathway[i]
         p.goto(pos_to_sc(x, y))
         p.stamp()
 
+
 def reset_path(pathway):
-    #Reset route drawn
+    # Reset route drawn
     p = Block("black")
-    for i in range(1 , len(pathway)-1):
+    for i in range(1, len(pathway) - 1):
         x, y = pathway[i]
         p.goto(pos_to_sc(x, y))
         p.stamp()
+
 
 path_astar = [(y, x) for (x, y) in jalurAStar]
 path_bfs = [(y, x) for (x, y) in bfs(start, finish)]
